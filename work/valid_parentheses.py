eeb3b51f2b2e202f421fcb3de1038b86126de64c
Naoki Wayama <n.wayama0402@gmail.com>
2023-06-11 14:44:55 +0900

    work valid

---
+++ work/valid_parentheses.py
@@ -0,0 +1,40 @@
+#  '(', ')', '{', '}', '[' ']'の組み合わせ判定
+
+
+# Input: s = "()"
+# Output: true
+
+# Input: s = "()[]{}"
+# Output: true
+
+# Input: s = "(]"
+# Output: false
+
+
+def is_valid(s: str) -> bool:
+    data = list(s[::])
+    if len(data) % 2 != 0:
+        return False
+
+    tmp_list = []
+    for val in data:
+        if val in ["(", "{", "["]:
+            tmp_list.append(val)
+            continue
+
+        if tmp_list == []:
+            return False
+
+        tmp = f"{tmp_list.pop(-1)}{val}"
+        if tmp not in ["{}", "()", "[]"]:
+            return False
+
+    return True if tmp_list == [] else False
+
+
+print(is_valid("()"))
+print(is_valid("()[]{}"))
+print(is_valid("(]"))
+print(is_valid("{[]}"))
+print(is_valid("([)]"))
+print(is_valid("(("))
